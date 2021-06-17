af98314b6efecf1ae6b222a82b2258a4a97c8de0
roikoren755 <26850796+roikoren755@users.noreply.github.com>
2021-06-17 16:17:34 +0300

    feat: circleci (#1)
    * Add .circleci/config.yml
    
    * Try this
    
    * Try this
    
    * Run yarn
    
    * Change node versions
    
    * Fix node versions
    
    * Try more stuffs
    
    * Small fixes and coveralls
    
    * No more coveralls
    
    * Update readme
    
    * Fix branch name
    
    * Add publish job to main
    
    * Rename again
    
    * Fix build
    
    * Run tests
    
    * Run tests :facepalm:
    
    * Will probably work :fingers-crossed:
    
    * Updated config.yml
    
    * Run yarn type too

--- src/query.ts
+++ src/query.ts
@@ -1,12 +1,21 @@
 import { createHash } from 'crypto';
 
-import type { BuilderInput, IParamAggregator, QueryCompiler, QueryParameters, ValueType } from './interfaces';
+import type {
+  BuilderInput,
+  IParamAggregator,
+  QueryCompiler,
+  QueryParameters,
+  ValueType,
+} from './interfaces';
 
-const createName = (queryText: string): string => createHash('sha256').update(queryText).digest().toString('base64');
+const createName = (queryText: string): string =>
+  createHash('sha256').update(queryText).digest().toString('base64');
 
-const createAggregator = <T>(params: QueryParameters<T>): IParamAggregator<T> => {
+const createAggregator = <T, U>(
+  params: QueryParameters<QueryCompiler<T, U>>
+): IParamAggregator<T, U> => {
   const values: ValueType[] = [];
-  const keyMap: Partial<Record<keyof QueryParameters<T>, string>> = {};
+  const keyMap: Partial<Record<keyof U, string>> = {};
 
   return {
     get props() {
@@ -16,7 +25,7 @@ const createAggregator = <T>(params: QueryParameters<T>): IParamAggregator<T> =>
 
       return values;
     },
-    key(target) {
+    key<K extends keyof U>(target: K) {
       if (!keyMap[target]) {
         const index = values.push((params[target] as unknown) as ValueType);
 
@@ -25,10 +34,12 @@ const createAggregator = <T>(params: QueryParameters<T>): IParamAggregator<T> =>
 
       return keyMap[target]!;
     },
-    values(args) {
-      const items: ValueType[] = Array.isArray(args) ? args : params[args];
+    values<K extends keyof U>(args: K | ValueType[]) {
+      const items = Array.isArray(args)
+        ? args
+        : ((params[args] as unknown) as ValueType[]);
 
-      return items.map((item) => `$${values.push(item)}`).join(',');
+      return items.map((item) => `$${values.push(item)}`).join(",");
     },
     value(item) {
       return `$${values.push(item)}`;
@@ -38,18 +49,22 @@ const createAggregator = <T>(params: QueryParameters<T>): IParamAggregator<T> =>
 
 export const query = <T, K>(
   template: TemplateStringsArray,
-  ...args: BuilderInput<QueryCompiler<T, K>>[]
+  ...args: BuilderInput<T, K>[]
 ): QueryCompiler<T, K> => ({
   compile(compileValues) {
-    const agg = createAggregator<QueryCompiler<T, K>>(compileValues);
+    const agg = createAggregator<T, K>(compileValues);
 
     const parts = Array.from(template);
 
     const text = args.reduce<string>((acc, arg) => {
-      const target = (typeof arg === 'function' ? arg(agg, compileValues) : arg) as string;
+      const target = (typeof arg === "function"
+        ? arg(agg, compileValues)
+        : arg) as string;
 
-      return `${acc}${target in compileValues ? agg.key(target as keyof K) : target}${parts.shift()}`;
-    }, parts.shift() ?? '');
+      return `${acc}${
+        target in compileValues ? agg.key(target as keyof K) : target
+      }${parts.shift()}`;
+    }, parts.shift() ?? "");
 
     const name = createName(text);
 
@@ -69,7 +84,7 @@ export const extend = <
   change: {
     from?: { [R in keyof T]: (...args: T[R]) => L };
     to?: { [R in keyof U]: (raw: K) => U[R] };
-  },
+  }
 ): QueryCompiler<K, L, M & T, N & U> =>
   ({
     ...input,
