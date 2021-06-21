4cb2c6138d078b8a05b483a4b1d93948ca0fb727
Dmitry Dodzin <d.dodzin@gmail.com>
2021-06-21 12:54:05 +0300

    Add correct coverage settings

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,15 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}'
+  ],
+  coverageReporters: [
+    'lcov',
+    'text',
+    'text-summary'
+  ],
   reporters: [
     'default',
     ['jest-junit', { suiteName: 'norm tests', outputDirectory: './reports', outputName: 'norm.xml' }],
